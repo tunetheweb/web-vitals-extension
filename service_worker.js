0c09e904ec0031b919a0bbe5a1476597521a0ea7
Barry Pollard <barrypollard@google.com>
2023-04-12 23:39:50 +0100

    Take 25

--- service_worker.js
+++ service_worker.js
@@ -276,7 +276,7 @@ async function animateBadges(request, tabId) {
 
 // message from content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Hi Barry', request, sender, sendResponse);
+  if (sender.documentLifecycle === 'cached') return;
   if (request.passesAllThresholds !== undefined) {
     // e.g passesAllThresholds === 'GOOD' => green badge
     animateBadges(request, sender.tab.id);
